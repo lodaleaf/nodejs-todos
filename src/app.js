ecb62c5686984ddb0f3f56abc2e00ff713b3c6fb
Chaiyaphong Boonkhlerm <chaiyaphong.boonkhlerm@krungthai-axa.co.th>
2018-04-05 14:16:12 +0700

    change eslint config for tab = 2 spaces

--- src/app.js
+++ src/app.js
@@ -8,73 +8,73 @@ const app = express()
 
 app.use(bodyParser.json())
 app.get('/todos', async (req, res) => {
-    const todos = await sequelize.transaction(transactionOptions, (t) => {
-        return Todo.findAll({
-            transaction: t
-        })
+  const todos = await sequelize.transaction(transactionOptions, (t) => {
+    return Todo.findAll({
+      transaction: t
     })
-    res.send(todos)
+  })
+  res.send(todos)
 })
 app.get('/todos/:id', async (req, res) => {
-    const todoId = req.params.id
-    const todo = await sequelize.transaction(transactionOptions, (t) => {
-        return Todo.findOne({
-            where: {
-                id: todoId
-            },
-            transaction: t
-        })
+  const todoId = req.params.id
+  const todo = await sequelize.transaction(transactionOptions, (t) => {
+    return Todo.findOne({
+      where: {
+        id: todoId
+      },
+      transaction: t
     })
-    res.send(todo)
+  })
+  res.send(todo)
 })
 app.post('/todos', async (req, res) => {
-    console.log(req.body)
-    const todo = await sequelize.transaction(transactionOptions, (t) => {
-        return Todo.create(req.body, {
-            transaction: t
-        })
+  console.log(req.body)
+  const todo = await sequelize.transaction(transactionOptions, (t) => {
+    return Todo.create(req.body, {
+      transaction: t
     })
-    res.send(todo)
+  })
+  res.send(todo)
 })
 app.delete('/todos/:id', async (req, res) => {
-    const todoId = req.params.id
-    await sequelize.transaction(transactionOptions, (t) => {
-        return Todo.destroy({
-            where: {
-                id: todoId
-            },
-            transaction: t
-        })
+  const todoId = req.params.id
+  await sequelize.transaction(transactionOptions, (t) => {
+    return Todo.destroy({
+      where: {
+        id: todoId
+      },
+      transaction: t
     })
-    res.sendStatus(200)
+  })
+  res.sendStatus(200)
 })
 app.put('/todos/:id', async (req, res) => {
-    const todoId = req.params.id
-    const todo = req.body
-    sequelize.transaction(transactionOptions, (t) => {
-        return Todo.update(
-            {
-                name: todo.name,
-                doen: todo.done
-            },
-            {
-                where: { id: todoId },
-                transaction: t
-            }
-        )
-    }).then(async () => {
-        const todo = await sequelize.transaction(transactionOptions, (t) => {
-            return Todo.findOne({
-                where: {
-                    id: todoId
-                },
-                transaction: t
-            })
-        })
-        res.send(todo)
+  const todoId = req.params.id
+  const todo = req.body
+  sequelize.transaction(transactionOptions, (t) => {
+    return Todo.update(
+      {
+        name: todo.name,
+        doen: todo.done
+      },
+      {
+        where: { id: todoId },
+        transaction: t
+      }
+    )
+  }).then(async () => {
+    const todo = await sequelize.transaction(transactionOptions, (t) => {
+      return Todo.findOne({
+        where: {
+          id: todoId
+        },
+        transaction: t
+      })
     })
+    res.send(todo)
+  })
 })
 
 app.listen(3000, () => {
-    console.info('server is listening to 3000')
+  console.info('server is listening to 3000')
 })
