ea60d9a3840ef0105b4b630d428abfc984e2867d
Chaiyaphong Boonkhlerm <chaiyaphong.boonkhlerm@krungthai-axa.co.th>
2018-04-05 12:12:26 +0700

    put delete post todos api

--- src/app.js
+++ src/app.js
@@ -15,14 +15,64 @@ app.get('/todos', async (req, res) => {
     })
     res.send(todos)
 })
-app.get('/todos/:id', (req, res) => {
-    res.send('hello')
+app.get('/todos/:id', async (req, res) => {
+    const todoId = req.params.id
+    const todo = await sequelize.transaction(transactionOptions, (t) => {
+        return Todo.findOne({
+            where: {
+                id: todoId
+            },
+            transaction: t
+        })
+    })
+    res.send(todo)
 })
-app.post('/todos', (req, res) => {
-    res.send('hello')
+app.post('/todos', async (req, res) => {
+    console.log(req.body)
+    const todo = await sequelize.transaction(transactionOptions, (t) => {
+        return Todo.create(req.body, {
+            transaction: t
+        })
+    })
+    res.send(todo)
 })
-app.delete('/todos', (req, res) => {
-    res.send('hello')
+app.delete('/todos/:id', async (req, res) => {
+    const todoId = req.params.id
+    await sequelize.transaction(transactionOptions, (t) => {
+        return Todo.destroy({
+            where: {
+                id: todoId
+            },
+            transaction: t
+        })
+    })
+    res.sendStatus(200)
+})
+app.put('/todos/:id', async (req, res) => {
+    const todoId = req.params.id
+    const todo = req.body
+    sequelize.transaction(transactionOptions, (t) => {
+        return Todo.update(
+            {
+                name: todo.name,
+                doen: todo.done
+            },
+            {
+                where: { id: todoId },
+                transaction: t
+            }
+        )
+    }).then(async () => {
+        const todo = await sequelize.transaction(transactionOptions, (t) => {
+            return Todo.findOne({
+                where: {
+                    id: todoId
+                },
+                transaction: t
+            })
+        })
+        res.send(todo)
+    })
 })
 
 app.listen(3000, () => {
