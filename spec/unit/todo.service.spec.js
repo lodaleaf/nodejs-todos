57f01be3ba4fa93fca612fe781e353d10b3ba9e4
Chaiyaphong Boonkhlerm <chaiyaphong.boonkhlerm@krungthai-axa.co.th>
2018-04-05 16:46:03 +0700

    Add valdiation to not allow creating todo before 8AM

--- spec/unit/todo.service.spec.js
+++ spec/unit/todo.service.spec.js
@@ -1,8 +1,11 @@
 const TodoDao = require('../../src/dao/TodoDao')
 const TodoService = require('../../src/service/TodoService')
+const moment = require('moment')
 describe('todo service', () => {
   describe('create todo', () => {
     it('should call todoDao.service', async () => {
+      const freezeTime = moment().hour(10).minutes(0).second(0).millisecond(0)
+      jasmine.clock().mockDate(freezeTime.toDate())
       const todoStub = {
         name: 'asdf',
         done: false
@@ -12,6 +15,24 @@ describe('todo service', () => {
       const todoService = new TodoService(todoDao)
       await todoService.createTodo(todoStub)
       expect(todoDao.createTodo).toHaveBeenCalledWith(todoStub)
+    })
+
+    it('should not allow to create todo before 8:00 AM', async () => {
+      const freezeTime = moment().hour(8).minutes(0).second(0).millisecond(0)
+      jasmine.clock().mockDate(freezeTime.toDate())
+
+      const todoStub = {
+        name: 'name',
+        done: false
+      }
+      const resultStub = {
+        name: 'name'
+      }
+      const todoDao = new TodoDao()
+      spyOn(todoDao, 'createTodo').and.returnValue(Promise.resolve(resultStub))
+      const todoService = new TodoService(todoDao)
+      await todoService.createTodo(todoStub)
+      expect(todoDao.createTodo).not.toHaveBeenCalled()
     })
   })
 })
--- src/service/TodoService.js
+++ src/service/TodoService.js
@@ -1,9 +1,14 @@
+const moment = require('moment')
+
 class TodoService {
   constructor (todoDao) {
     this.todoDao = todoDao
   }
   async createTodo (todo) {
-    return await this.todoDao.createTodo(todo)
+    const freezeTime = moment().hour(8).minutes(0).second(0).millisecond(0)
+    if (moment().isAfter(freezeTime)){
+      return await this.todoDao.createTodo(todo)
+    }
   }
 }
 
